9d34b3c398ea4ec6d4076e4977c4324f668bcb90
kbrawnier <kendontbrawner@gmail.com>
2020-07-28 19:10:12 +0000

    backend

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class Jumbotron extends React.Component {
   render() {
-    const {image, title, subtitle, text} = this.props;
+    const {image, title, subtitle, text, book_image} = this.props;
     return <div>
       <div className="pv5 pv6-l ph3 bg-center cover" style={{
         backgroundImage: image && `url(${image})`
@@ -19,6 +19,9 @@ export default class Jumbotron extends React.Component {
             </div>
             <div className="mw7 relative bg-fix-primary">
               {text && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{ text }</p>}
+            </div>
+            <div>
+                <img src = "{book_image}" alt=""/>
             </div>
           </div>
         </div>
